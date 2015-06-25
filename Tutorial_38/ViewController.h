6cccb529898415072fb8e446141ae00e9bfb1fe0
rewfe <qrewfe@yandex.ru>
2015-06-26 02:28:49 +0300

    Initial Commit

---
+++ Tutorial_38/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tutorial_38
+//
+//  Created by Admin on 26.06.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
